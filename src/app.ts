0ca7cbab9218e99c3d1e2fae5776eac83a911018
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-07-16 23:54:49 +0200

    instal useful package

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
+import express, { Application, Response, Request } from "express";
+import dotenv from "dotenv";
+const app: Application = express();
+
+dotenv.config({ path: ".env" });
+
 const hello: string = "HELLO WORLD";
+
+app.get("/", (_req: Request, res: Response) => {
+	res.send(hello);
+});
+
+const PORT = process.env.PORT || 4040;
+app.listen(PORT, () => {
+	console.log("Server running on port" + PORT);
+});
