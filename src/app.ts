3abb9b2c5c897037147d8a6c95831a1e15e7fb64
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-10-09 11:13:22 +0200

    delete using public folder

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,6 @@ dotenv.config({ path: ".env" });
 
 app.use(cors());
 
-app.use(express.static("public"));
-
 app.get("/dataBandcamp", async (_req: Request, res: Response) => {
 	const dataFromBandcamp = await getDataFromBandcamp();
 	res.send(dataFromBandcamp);
