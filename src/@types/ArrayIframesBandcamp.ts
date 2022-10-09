50e931d854e4cb6f3c26caaeb036197676dff0cd
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-10-09 10:48:07 +0200

    create simple backend with puppeteer backend for sound Architecture

---
+++ src/@types/ArrayIframesBandcamp.ts
@@ -0,0 +1,6 @@
+export interface InfoBandcampIframe {
+    iframeLink: string;
+    mainTitles: Array<string>;
+}
+
+export type ArrayIframesBandcamp = Array<InfoBandcampIframe>;
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express, { Application, Response, Request } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { getDataFromBandcamp } from "./utils/getDataFromBandcamp";
 const app: Application = express();
 
 dotenv.config({ path: ".env" });
 
 app.use(cors());
 
-app.get("/", (_req: Request, res: Response) => {
-	res.send("");
+app.get("/dataBandcamp", async (_req: Request, res: Response) => {
+	const dataFromBandcamp = await getDataFromBandcamp();
+	res.send(dataFromBandcamp);
 });
 
 const PORT = process.env.PORT || 4040;
