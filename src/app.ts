72900c538e373f6726e68f25f870b4b0fda44cfd
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-10-09 11:20:19 +0200

    checking deploy heroku

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Application, Response, Request } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import chromium from "chrome-aws-lambda";
 import { getDataFromBandcamp } from "./utils/getDataFromBandcamp";
+import { ArrayIframesBandcamp } from "./@types/ArrayIframesBandcamp";
+import { puppeteerConfig } from "./config/puppeteerConfig";
+import { websitesUrl } from "./config/websitesUrl";
 const app: Application = express();
 
 dotenv.config({ path: ".env" });
@@ -9,8 +13,60 @@ dotenv.config({ path: ".env" });
 app.use(cors());
 
 app.get("/dataBandcamp", async (_req: Request, res: Response) => {
-	const dataFromBandcamp = await getDataFromBandcamp();
-	res.send(dataFromBandcamp);
+	const iframes: ArrayIframesBandcamp = [];
+	const { urlBandcamp } = websitesUrl;
+
+	const browser = await chromium.puppeteer.launch(await puppeteerConfig());
+	const page = await browser.newPage();
+
+	await page.goto(urlBandcamp);
+
+	const allAlbumsUser = await page.$$("#music-grid > li > a");
+
+	for (let i = 0; i <= allAlbumsUser.length - 1; i++) {
+		const allAlbums = await page.$$("#music-grid > li > a");
+
+		await allAlbums[i].click();
+		await page.waitForNavigation();
+
+		const tracks = await page.$$("td > .title > a");
+		const tracksLength = tracks.length;
+
+		const titlesAllTracks: Array<string> = [];
+
+		for (let i = 0; i <= tracksLength - 1; i++) {
+			const trackTitle = await page.evaluate((element) => {
+				return element?.innerText;
+			}, tracks[i]);
+			titlesAllTracks.push(trackTitle);
+		}
+
+		const shareButton = await page.$(".share-embed");
+		await shareButton?.click();
+
+		const buttonEmbed = await page.$(".embed-other-services.panel-section  > a");
+		await buttonEmbed?.click();
+
+		const bigSizeElement = await page.$(".sizechoice.large > .sizepreview");
+		await bigSizeElement?.click();
+
+		const checkbox = await page.$('label > input[data-bind="checked: show_tracklist"]');
+		await checkbox?.click();
+
+		const inputElement = await page.$("input.embed_text");
+
+		const dataIframe = await page.evaluate((element) => {
+			return element?.value;
+		}, inputElement);
+
+		iframes.push({ iframeLink: dataIframe, mainTitles: titlesAllTracks });
+
+		await page.goto(urlBandcamp);
+	}
+
+	await browser.close();
+
+	res.send(iframes);
 });
 
 const PORT = process.env.PORT || 4040;
